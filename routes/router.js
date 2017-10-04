7ac9d02e3c6066d851445bbfebc7d3045cbbf8c6
grillopress <grillopress@gmail.com>
2017-10-04 12:16:05 +0100

    create improved prototype F index page

--- routes/router.js
+++ routes/router.js
@@ -11,24 +11,18 @@ router.get('/home', function(req, res, next) {
   res.render( 'index' );
 });
 
-// // // // // // //
-// v1 Directory Route Code
-//
-// Copy this and replace with v2 etc. to recreate
-// // // // // // //
-
 // GET v1 index page.
-router.get('/v1', function(req, res, next) {
-  res.render( 'v1/index' );
+router.get('/F', function(req, res, next) {
+  res.render( 'F/index' );
 });
 
 // GET all v1 URL reqs and push them to a template in the v1 file
 // This doesn't handle sub-urls yet...
 // This feels really brittle and hacky...
 // No handling of no view found...
-router.get('/v1/:view', function(req, res, next) {
+router.get('/F/:view', function(req, res, next) {
   var theView = req.params.view;
-  res.render( 'v1/' + theView );
+  res.render( 'F/' + theView );
 });
 
 // // // // // // //
